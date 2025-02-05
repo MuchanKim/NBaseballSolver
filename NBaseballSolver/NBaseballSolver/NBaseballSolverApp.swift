942e9b342b63b499357c048eab38519b88cd0d86
Moo <mutopia82@gmail.com>
2025-02-06 00:29:43 +0900

    Initial commit

---
+++ NBaseballSolver/NBaseballSolver/NBaseballSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBaseballSolverApp.swift
+//  NBaseballSolver
+//
+//  Created by Muchan Kim on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NBaseballSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
